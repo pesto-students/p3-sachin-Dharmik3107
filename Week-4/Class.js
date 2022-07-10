6bd4949b0b6b88262a8e36ed654a1c2772ffd24a
Dharmik3107 <49364985+Dharmik3107@users.noreply.github.com>
2022-07-10 12:16:39 +0530

    Update Class.js

--- Week-4/Class.js
+++ Week-4/Class.js
@@ -20,29 +20,3 @@ class Teacher extends person{
 const teacher = new Teacher('Dharmik','Maths');
 teacher.teach();
 teacher.printName();
-
-
-
-
-//! ES5 Based Inheritance using function
-
-function PERSON(NAME){
-    this.NAME = NAME;
-}
-PERSON.prototype.printname = function(){
-    console.log(this.NAME)
-}
-function TEACHER(NAME,SUBJECT){
-            PERSON.call(this, NAME);
-            this.SUBECT = SUBJECT;
-}
-
-TEACHER.prototype.TEACH = function(){
-    console.log(this.NAME + ' is now teaching ' +this.SUBECT)
-}
-
-Object.setPrototypeOf(TEACHER.prototype,PERSON.prototype)
-
-const TeaCher = new TEACHER('Suresh','Chemistry');
-TeaCher.TEACH();
-TeaCher.printname();
