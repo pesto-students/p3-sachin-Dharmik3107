7fd10f9766982889cda6f72b841d9188388656cd
Dharmik3107 <49364985+Dharmik3107@users.noreply.github.com>
2022-07-29 17:10:32 +0530

    checkParenthesis.js

--- Week-7/checkParenthesis.js
+++ Week-7/checkParenthesis.js
@@ -1,7 +1,8 @@
 //check balancing of paranthesis: Balanced : false && Not Balanced : True
 const MyStack = require("./customStack");
-
+//function to check balanced paranthesis
 function checkParanthesis(expression, inputValue) {
+  //necessory input validation 
   if (typeof expression !== "string") throw "Invalid Input";
   let paranthesis = ["(", "{", "[", ")", "}", "]"];
   let checker = 0;
@@ -10,19 +11,26 @@ function checkParanthesis(expression, inputValue) {
     if (paranthesis.includes(element)) checker += 1;
     if (checker == 0) throw "Invalid String";
   }
+  //creating a new stack
   let stack = new MyStack();
+  //based on input value loop will run
   if (inputValue === "(") outputValue = ")";
   if (inputValue === "{") outputValue = "}";
   if (inputValue === "[") outputValue = "]";
+  //loop to check paranthesis using counter
   for (let element of expression) {
       if (element == outputValue) {
         let top = stack.pop();
+        //when output value found counter +1
         let counter = 1;
         while (stack.size !== 0) {
           let top = stack.pop();
+          //when output value found counter +1
           if (top == outputValue) counter += 1;
+          //when output value found counter -1 
           if (top == inputValue) counter -= 1;
         }
+        //if counter = 0 then it is balanced
         if (counter !== 0) return true;
       }else{
         stack.push(element);
@@ -32,12 +40,12 @@ function checkParanthesis(expression, inputValue) {
 }
 try{
     newString = "[(])";
+    //invoking function for '('
     const getChecked1 = checkParanthesis(newString,'(');
-    console.log(getChecked1)//false
+    //invoking function for '{'
     const getChecked2 = checkParanthesis(newString,'{');
-    console.log(getChecked2)//false
+    //invoking function for '['
     const getChecked3 = checkParanthesis(newString,'[');
-    console.log(getChecked3)//false
     if(getChecked1 == false && getChecked2 == false && getChecked3 == false) console.log(false);
     else return console.log(true);
 }catch(e){
