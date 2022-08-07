50ab7e085d7540dd5308e90e653f235ffb120ed2
Dharmik Abhangi <abhangidharmik@gmail.com>
2022-08-07 08:33:27 +0530

    Assignment 8.2 modified(removed debugging logs)

--- Week-8/Assignment 8.2.js
+++ Week-8/Assignment 8.2.js
@@ -4,16 +4,13 @@ const validateTree = (tree) => {
   if (tree.root == null) throw "Empty Tree";
   if (!tree.root.left && !tree.root.right) return "Tree is Validated";
   let current = tree.root;
-  console.log("loop called");
   while (current.left) {
     let currentValue = current.value;
     let leftNode = current.left;
     let leftValue = leftNode.value;
-    console.log("to left", currentValue, leftValue);
     if (leftValue > currentValue) return "left Tree is not validated";
     current = current.left;
     if(current.right){
-        console.log('Checking right in left', current.value,current.right.value)
         if(current.value > current.right.value) return 'left tree is not validated'
     }
   }
@@ -22,15 +19,12 @@ const validateTree = (tree) => {
     let currentValue = current.value;
     let rightNode = current.right;
     let rightValue = rightNode.value;
-    console.log("to right", currentValue, rightValue,'current Right', current.right.value);
     if (rightValue < currentValue) return "right Tree is not validated";
     current = current.right;
     if(current.left){
-        console.log('Checking left in right', current.value,current.left.value)
         if(current.value < current.left.value) return 'right tree is not validated'
     }
   }
-  console.log(current.left,current.right)
   if (current.left == null && current.right == null) return "Tree is Validated";
 };
 try {
