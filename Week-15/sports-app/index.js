df9ae016c45973e7d604b2c6b8b75b0c82ff4538
Dharmik Abhangi <abhangidharmik@gmail.com>
2022-10-20 19:03:10 +0530

    user authentication api complete

--- Week-15/sports-app/index.js
+++ Week-15/sports-app/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 require('dotenv').config();
-
+const userRouter = require('./routes/user');
+const connectDatabase = require('./database/database')
 const app = express();
+connectDatabase();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
-app.use('/',(req,res)=>{
-    res.json({
-        messsage:'connected'
-    })
-})
+app.use('/user',userRouter)
 
 app.listen(process.env.PORT || 5000, ()=>{
     console.log(`Server is listening on port ${process.env.PORT}`);
