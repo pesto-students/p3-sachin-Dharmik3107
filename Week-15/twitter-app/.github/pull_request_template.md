6a3ecf1033c411d80d95ce36efa60bd250de143e
Dharmik Abhangi <abhangidharmik@gmail.com>
2022-11-13 07:54:50 +0530

    Twitter api

---
+++ Week-15/twitter-app/.github/pull_request_template.md
@@ -0,0 +1,16 @@
+## Description
+
+## Issue Addressed (if applicable)
+_Issue number being fixed (Format: Fixes #xxx)_
+
+## Type
+<!--- What types of changes does your code introduce? Put an `x` in all options that apply. -->
+- [ ] Bug fix (non-breaking change which fixes an issue)
+- [ ] New feature (non-breaking change which adds functionality)
+- [ ] Breaking change (fix or feature that would cause existing functionality to change)
+
+### Screenshots (if applicable):
+
+<!-- ### How Has This Been Tested?
+- [ ] I have added tests to cover my changes.
+- [ ] All new and existing tests passed. -->
--- Week-15/twitter-app/index.js
+++ Week-15/twitter-app/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
-require('dotenv').config();
+const twitter = require('twitter');
 
-const app = express();
+const client = new twitter({
+    consumer_key:'',
+    consumer_secret:'',
+    access_token_key:'',
+    access_token_secret:''
+});
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-app.listen(process.env.PORT , ()=>{
-    console.log(`Server started on port ${process.env.PORT}`);
+const params = {screenName:'nodejs'};
+client.get("statuses/user_timeline",params, function(error, tweets, response ){
+    if(!error) {
+        console.log(tweets)
+    }
 })
